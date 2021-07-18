2752ecb916f22cc072b61786dccc428c6356c0db
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-07-18 18:14:15 +0200

    Create Example2.c
    Input two numbers (start and finish) and print out only the numbers in which the Most Significant Digit is an even number and is greater than the Least Significant Digit

---
+++ Example2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int start, finish, digit;
+    int firstDigit, lastDigit;
+    printf ("Enter start value: "); scanf("%d", &start);
+    printf ("Enter finish value: "); scanf("%d", &finish);
+    for (start; start <= finish; start++)
+    {
+        int temp = start;
+        int digitCounter=0;
+        while (temp != 0)
+        {
+            digit = temp % 10;
+            temp = temp/10;
+        }
+        firstDigit = digit;
+        lastDigit = start % 10;
+        if ((firstDigit % 2 == 0) && (firstDigit > lastDigit))
+            printf ("%d\n", start);
+    }
+    return 0;
+}
