76bccd3beb45ce0c2cf1ef7cc2eb3d6ff6a9189c
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-07-18 18:41:19 +0200

    Update Example5.c
    Input unknown amount of numbers and find the maximum and minimum value

--- Cycles/Example5.c
+++ Cycles/Example5.c
@@ -14,6 +14,6 @@ int main()
         if (number < min)
             min = number;
     }
-    printf ("Maximum number iS: %d\nMinimum number is: %d", max, min);
+    printf ("Maximum number is: %d\nMinimum number is: %d", max, min);
     return 0;
 }
