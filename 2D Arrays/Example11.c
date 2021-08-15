6effde5a82c63c37574d1c8984156c55b40a010d
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-08-15 16:14:26 +0200

    Create Example11.c
    Find the sum of all the elements in the matrix and print the number on the left half of the main diagonal

---
+++ 2D Arrays/Example11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, m;
+    int sum=0;
+    printf ("Enter number of rows: "); scanf("%d", &n);
+    printf ("Enter number of columns: "); scanf("%d", &m);
+    int a[n][m];
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            printf ("a [ %d ][ %d ] = ",i,j);
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if (j < i)
+                printf ("%d      ", sum);
+            else
+                printf ("%d      ", a[i][j]);
+        }
+        printf ("\n");
+    }
+    return 0;
+}
