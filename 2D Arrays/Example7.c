455b51f4ff6c2445b5cdd3584ab0e61c4e6edc64
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-08-15 15:51:30 +0200

    Create Example7.c
    Write a matrix that will print out only the elements of the main and opposite diagonal's elements

---
+++ 2D Arrays/Example7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, m;
+    printf ("Enter number of rows: "); scanf("%d", &n);
+    printf ("Enter number of columns: "); scanf("%d", &m);
+    int a[n][m];
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            printf ("a [ %d ][ %d ] = ",i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if (i==j || (i+j==m-1))
+                printf ("%d     ", a[i][j]);
+            else
+                printf ("     ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
