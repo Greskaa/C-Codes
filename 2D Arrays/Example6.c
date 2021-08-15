407a81d73eefc3a65aaa3a2976dfe6439f039e14
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-08-15 15:41:11 +0200

    Create Example6.c
    Find the sum of the element's positions (one position yes one no) and print out the matrix where the opposite diagonal is the sum of those positions

---
+++ 2D Arrays/Example6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, m;
+    printf ("Enter number of rows: "); scanf("%d", &n);
+    printf ("Enter number of columns: "); scanf("%d", &m);
+    int a[n][m];
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            printf ("a [ %d ][ %d ] = ",i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int sum=0;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if ((i%2==0) && (j%2!=0))
+                sum = sum + a[i][j];
+            else if ((i%2!=0) && (j%2==0))
+                sum = sum + a[i][j];
+        }
+    }
+
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<m; j++)
+        {
+            if (i+j == n-1)
+                printf ("%d ", sum);
+            else
+                printf ("%d ", a[i][j]);
+        }
+        printf ("\n");
+    }
+
+    return 0;
+}
