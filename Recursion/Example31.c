ab829007a985388b26f8d39c8358fded77321548
Gresa Kadriu <65871981+Greskaa@users.noreply.github.com>
2021-08-22 01:45:26 +0200

    Create Example31.c
    Enter a start and finish value and print out only the numbers whose First and Last Digits are Even

---
+++ Recursion/Example31.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int firstDigit(int number)
+{
+    if (number < 10)
+        return number;
+    else
+        return firstDigit(number/10);
+}
+int lastDigit(int number)
+{
+    if (number)
+        return number%10;
+}
+void readNumbers(int start, int finish)
+{
+    if (firstDigit(start)%2==0 && lastDigit(start)%2==0)
+        printf ("%d\n", start)
+    if (start == finish)
+        return;
+    return readNumbers(start+1, finish);
+}
+
+int main()
+{
+    int a, b;
+    printf ("Enter start value: "); scanf("%d", &a);
+    printf ("Enter finish value: "); scanf("%d", &b);
+    readNumbers(a, b);
+    return 0;
+}
